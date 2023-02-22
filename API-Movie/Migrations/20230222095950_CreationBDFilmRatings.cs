520ef17fcac26babc432def34d2e8d1e762bd49a
Tristan-La <tristan.lambert@etu.univ-smb.fr>
2023-02-22 11:11:30 +0100

    ça marche mieux wola

---
+++ API-Movie/Migrations/20230222095950_CreationBDFilmRatings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Movie.Migrations
+{
+    public partial class CreationBDFilmRatings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
